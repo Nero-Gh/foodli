af665321d8fcccb00c71299094a8a21879e7f745
nerogh <bismark.kadogbe@amalitech.com>
2024-09-04 23:29:14 +0000

    feat:added footers

---
+++ src/app/pages/home/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ImageComponent } from '../../../../shared/components/image/image.component';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [ImageComponent],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
--- src/app/pages/home/pages/home/home.component.ts
+++ src/app/pages/home/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { NavbarComponent } from '../../../../shared/components/navbar/navbar.com
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
 import { ServeComponent } from '../../components/serve/serve.component';
 import { MenuComponent } from '../../components/menu/menu.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ import { MenuComponent } from '../../components/menu/menu.component';
     HeroSectionComponent,
     ServeComponent,
     MenuComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
